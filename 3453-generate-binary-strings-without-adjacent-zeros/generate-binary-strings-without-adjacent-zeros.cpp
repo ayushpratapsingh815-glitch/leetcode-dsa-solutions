ea3df828b0a3c33f0a5f58eaa43c8127413cdcf9
Ayush Pratap Singh <ayushpratapsingh815@gmail.com>
2026-08-31 22:26:33 +0530

    Time: 11 ms (72.46%) | Memory: 20.8 MB (87.47%) - LeetSync

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> validStrings(int n) {
+        vector<string> res{"1","0"};
+        if(n == 1) {
+            return res;
+        }
+        for(int i=2; i<=n; i++) {
+            vector<string> temp;
+            for(int j=0; j<res.size(); j++) {
+                if(res[j].back() == '1') {
+                    temp.push_back(res[j]+"1");
+                    temp.push_back(res[j]+"0");
+                } else {
+                    temp.push_back(res[j]+"1");
+                }
+            }
+            res = temp;
+        }
+        return res;
+    }
+};
